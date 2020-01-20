a319ac8c3ff7b744e1a72c9c33f33cdf764c93bd
gayatri-bhadane <bhadanegayatri@gmail.com>
2020-01-20 17:19:07 +0000

    Modified FindElement & FindElementSuite file.
    Added Last and calculate_length function.

--- src/main/scala/FindElement.scala
+++ src/main/scala/FindElement.scala
@@ -1,8 +1,10 @@
+import scala.annotation.tailrec
+
 /**
  * Date: 17/01/2020
  * This file is created to practice some functions.
  *
- * Function Name: FindElement
+ * Function Name: nth
  * Purpose: This function accepts list and k number and returns kth element of list.
  * Eg. Inputs: num = 2 & nums = List(2,6,8,4)
  *     Output: 8
@@ -17,12 +19,40 @@ object FindElement  extends App {
       //This case executes recursion till the element found
       case num if(num > 0 && nums.length > 1) => nth(num-1, nums.tail)
       //This case will throw exception if none of the above case executed successfully.
-      case _ => throw  new NoSuchElementException
+      case _ => throw new NoSuchElementException
     }
   }
 
+  /**
+   * Function Name: last
+   * Purpose: This function accepts list and returns last element of list.
+   * @param input
+   * @tparam A
+   * @return last element of list
+   */
+  @tailrec
+  def last[A](input: List[A]) : A = input match {
+      case h :: Nil => h
+      case _ :: tail => last(tail)
+      case _ => throw new NoSuchElementException
+    }
+
+  /**
+   * Function Name: calculate_length
+   * Purpose: This function accepts list and accumulator and returns length of list.
+   * @param input
+   * @param calc_length
+   * @tparam A
+   * @return length of a list
+   */
+  @tailrec
+  def calculate_length[A](input: List[A], calc_length: Int) : Int= {
+      input match {
+        case Nil => calc_length
+        case x :: xs => calculate_length(xs, 1+ calc_length)
+      }
+    }
+
   val results = nth[Char](1, List('a', 'b', 'c', 'd') )
 
-  //Print results.
-  println(results)
 }
--- src/test/scala/FindElementSuite.scala
+++ src/test/scala/FindElementSuite.scala
@@ -3,9 +3,10 @@
  * This file is created to test some functions.
  *
  * Function Name: FindElementSuite
- * Purpose: This function tests success & failure case of nth function.
+ * Purpose: This function tests success & failure case of nth, last and calculate_length function.
  */
 import FindElement.nth
+import FindElement.last
 import org.scalatest.funsuite.AnyFunSuite
 
 class FindElementSuite extends AnyFunSuite{
@@ -19,5 +20,29 @@ class FindElementSuite extends AnyFunSuite{
     assertThrows[NoSuchElementException]{
       nth[Int](6, List(1, 5, 8, 9))
     }
+  }
+
+  /**
+   * This will test happy and failure case for last function.
+   */
+  test("Find the last element from the list."){
+    assert(last[Char](List('a', 'b', 'c', 'd') ) == 'd')
+  }
+
+  test("If we are trying to find out last element in empty list."){
+    assertThrows[NoSuchElementException]{
+      last(List())
+    }
+  }
+
+  /**
+   * This will test happy and failure case for calculate_length function.
+   */
+  test("Find the length of the list."){
+    assert(FindElement.calculate_length(List('a', 'b', 'c', 'd'), 0) == 4)
+  }
+
+  test("If we are trying to find out length of empty list."){
+    assert(FindElement.calculate_length(List(), 0) == 0)
   }
 }
